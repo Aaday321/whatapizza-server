e898698f75ecfaf1a6ad4b783f2af37ffc4fd2b4
Ade <aderitchards@gmail.com>
2026-01-17 04:35:32 -0800

    main

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-
-    return c.json({})
+app.get('/', (context) => {
+  console.log(
+    "We got a request!"
+  );
+  
+  context.status(202)
+  return context.text('What up, world!')
 })
 
 serve({
   fetch: app.fetch,
-  port: 4000
+  port: 3126
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
